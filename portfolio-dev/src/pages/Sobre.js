f7d8f21c19ccfc23e484d98ff6b6ee3959288ffd
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-10-18 11:26:51 -0300

    Adiciona conteúdo na página sobre

--- portfolio-dev/src/pages/Sobre.js
+++ portfolio-dev/src/pages/Sobre.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import Nav from '../components/nav';
+import nodeLogo from '../images/node.png';
+import mysqlLogo from '../images/mysql.png';
+import reactLogo from '../images/react.png';
+import mongoLogo from '../images/mongo.png';
 
 function About() {
   return (
     <div>
       <Nav />
-      Sou a página Sobre
+      <h2>
+        Um pouco sobre mim!
+      </h2>
+      <p>
+        Considero-me um entusiasta da tecnologia, principalmente computadores e programação, 
+        atualmente me encontro em transição de carreira, estudando desenvolvimento web 
+        full-stack pela <a href="https://www.betrybe.com">Trybe</a>
+      </p>
+      <h3>
+        Tecnologias de trabalho
+      </h3>
+      <section>
+        <div>
+          <img src={ nodeLogo } alt='Logo NodeJS' width="5%" />
+          <p>
+            Testes e desenvolvimento de aplicações e API's.
+          </p>
+        </div>
+        <div>
+          <img src={ mysqlLogo } alt='Logo mySQL' width="5%" />
+          <p>
+            Mais performance em suas queries.
+          </p>
+        </div>
+        <div>
+          <img src={ reactLogo } alt='Logo ReactJS' width="5%" />
+          <p>
+            Páginas ágeis para a sua estrutura front-end.
+          </p>
+        </div>
+        <div>
+          <img src={ mongoLogo } alt='Logo MongoDB' width="5%" />
+          <p>
+            Precisa de um banco de dados não relacional? 
+            MongoDB é um dos sistemas mais utilizados no mundo.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
