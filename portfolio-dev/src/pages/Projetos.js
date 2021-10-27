7c2012077a5335bf9a47e273c9ec7f94f8fdc3ea
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-10-27 12:00:16 -0300

    finaliza página de projetos

--- portfolio-dev/src/pages/Projetos.js
+++ portfolio-dev/src/pages/Projetos.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Nav from '../components/nav';
 import { Carousel } from 'react-bootstrap';
 import talkerProject from '../images/talker.png';
-import rickProject from '../images/rickAndMority.png';
+import rickProject from '../images/rickandmorty.png';
 import walletProject from '../images/wallet.png'
-import mysqlProject from '../images/mysqlwork.png'
+import mysqlProject from '../images/spotifyclone.png'
+import repoGit from '../images/repo.png';
+import walletLogo from '../images/walletLogo.png';
+import rickLogo from '../images/rickLogo.png';
 import '../css/projetos.css';
 
 function Projects() {
@@ -15,8 +18,70 @@ function Projects() {
         <h2>
           Sobre meus projetos
         </h2>
+      </section>
+      <section className="projectsRepo">
+        <h3>Trybe Wallet</h3>
+        <p>
+          <img src={ repoGit } width="1.5%" alt="Logo do repositório Git " />
+          <a 
+            href="https://github.com/Tarcisio-Menezes/trybeWallet"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para o repositório
+          </a>
+          <img src={ walletLogo } width="1.5%" alt="Logo da aplicação Wallet" />
+          <a 
+            href="https://tarcisio-menezes.github.io/trybeWallet/"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para a aplicação
+          </a>
+        </p>
+        <h3>Talker Manager</h3>
+        <p>
+          <img src={ repoGit } width="1.5%" alt="Logo do repositório Git " />
+          <a 
+            href="https://github.com/Tarcisio-Menezes/Project-Talker-Manager"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para o repositório
+          </a>
+        </p>
+        <h3>Rick and Morty Page</h3>
+        <p>
+          <img src={ repoGit } width="1.5%" alt="Logo do repositório Git " />
+          <a 
+            href="https://github.com/Tarcisio-Menezes/rickandmortpage"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para o repositório
+          </a>
+          <img src={ rickLogo } width="3%" alt="Logo da aplicação Rick and Morty page" />
+          <a 
+            href="https://tarcisio-menezes.github.io/rickandmortpage/"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para a aplicação
+          </a>
+        </p>
+        <h3>Spotify Simple Clone</h3>
+        <p>
+          <img src={ repoGit } width="1.5%" alt="Logo do repositório Git " />
+          <a 
+            href="https://github.com/Tarcisio-Menezes/MySql-One-for-all"
+            target="_blank" 
+            rel="noreferrer"
+          >
+            Link para o repositório
+          </a>
+        </p>
         <div className="slidesProjects">
-          <Carousel variant="dark">
+          <Carousel>
             <Carousel.Item>
               <img
                 className="d-block w-100"
@@ -50,7 +115,7 @@ function Projects() {
               <img
                 className="d-block w-100"
                 src={ rickProject }
-                alt="Third slide - Star Wars Planets"
+                alt="Three slide - Rick and Morty page"
               />
               <Carousel.Caption className ="rickP">
                 <h3>Rick and Morty page</h3>
@@ -64,10 +129,10 @@ function Projects() {
               <img
                 className="d-block w-100"
                 src={ mysqlProject }
-                alt="Six slide - Spotfy clone"
+                alt="Four slide - Spotfy clone"
               />
               <Carousel.Caption  className ="mysqlP">
-                <h3>Spotfy clone</h3>
+                <h3>Spotfy Clone</h3>
                 <p>
                 Spotify Simple Clone é uma série de comandos e queries 
                 que modela um banco de dados MySQL.
@@ -77,6 +142,17 @@ function Projects() {
           </Carousel>
         </div>
       </section>
+      <div className="moreJobs">
+        <h3> 
+          <a 
+            href="https://github.com/Tarcisio-Menezes"
+            target="_blank" 
+            rel="noreferrer"
+          > 
+            Conheça meus projetos mais atuais visitando minha página no gitHub
+          </a>
+        </h3>
+      </div>
     </div>
   );
 }
