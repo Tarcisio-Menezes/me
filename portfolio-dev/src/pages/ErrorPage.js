2d24b2377f1fbd7e2fc71ee9074e82fe4fae9985
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-10-27 12:08:36 -0300

    finaliza página not found

--- portfolio-dev/src/pages/ErrorPage.js
+++ portfolio-dev/src/pages/ErrorPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Nav from '../components/nav';
+import goku from '../images/goku.png';
 
 function ErrorPage() {
   return (
     <div>
       <Nav />
-      Sou a página de erro
+      <h2>
+        Ooops, não consegui encontrar esta rota!
+      </h2>
+      <img
+        src={ goku }
+        width="20%"
+        alt="Goku irritado por não encontrar o caminho"
+      />
     </div>
   );
 }
