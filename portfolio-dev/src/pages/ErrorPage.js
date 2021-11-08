7bf42e12120f22ee57048753ff0dd460daa9ed99
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-08 16:21:35 -0300

    adiciona responsividade em contact e errorPage

--- portfolio-dev/src/pages/ErrorPage.js
+++ portfolio-dev/src/pages/ErrorPage.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Nav from '../components/nav';
 import goku from '../images/goku.png';
+import '../css/errorPage.css';
 
 function ErrorPage() {
   return (
     <div>
       <Nav />
-      <h2>
-        Ooops, não consegui encontrar esta rota!
-      </h2>
-      <img
-        src={ goku }
-        width="20%"
-        alt="Goku irritado por não encontrar o caminho"
-      />
-      <footer className="footer">
+      <section className="errorPage">
+        <h2>
+          Ooops, não consegui encontrar esta rota!
+        </h2>
+        <img
+          src={ goku }
+          width="20%"
+          alt="Goku irritado por não encontrar o caminho"
+        />
+      </section>
+      <footer className="errorFooter">
         <p>
           Copyright © 2021 - Tarcísio Menezes Desenvolvedor Full-Stack
         </p>
