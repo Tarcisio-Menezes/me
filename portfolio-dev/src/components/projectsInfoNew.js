8bf3002e9890d94666d8738471c84ad37b5d9252
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-09 15:31:19 -0300

    transforma projectsInfoNew em cards

--- portfolio-dev/src/components/projectsInfoNew.js
+++ portfolio-dev/src/components/projectsInfoNew.js
@@ -1,45 +1,54 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 function projectsInfoNew() {
   return (
     <aside className="projectsRepo">
-      <h3>Rick and Morty Page</h3>
-      <article>
-        Procure pelos seus personagens favoritos e alguns episódios
-        do seriado Rick and Morty.
-      </article>
-      <div className="rickRepo">
-        <a
-          href="https://github.com/Tarcisio-Menezes/rickandmortpage"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p>Link para o repositório</p>
-        </a>
-      </div>
-      <div className="rickApp">
-        <a
-          href="https://tarcisio-menezes.github.io/rickandmortpage/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p>Link para a aplicação</p>
-        </a>
-      </div>
-      <h3>Spotify Simple Clone</h3>
-      <article>
-        Spotify Simple Clone é uma série de comandos e queries
-        que modela um banco de dados MySQL.
-      </article>
-      <div className="spotRepo">
-        <a
-          href="https://github.com/Tarcisio-Menezes/MySql-One-for-all"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p>Link para o repositório</p>
-        </a>
-      </div>
+      <Card style={ { width: '18rem' } }>
+        <Card.Body>
+          <Card.Title>Rick and Morty Page</Card.Title>
+          <Card.Text>
+            Procure pelos seus personagens favoritos e alguns episódios
+            do seriado Rick and Morty.
+          </Card.Text>
+          <div className="rickRepo">
+            <Card.Link
+              href="https://github.com/Tarcisio-Menezes/rickandmortpage"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Link para o repositório
+            </Card.Link>
+          </div>
+          <div className="rickApp">
+            <Card.Link
+              href="https://tarcisio-menezes.github.io/rickandmortpage/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Link para a aplicação
+            </Card.Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card style={ { width: '18rem' } }>
+        <Card.Body>
+          <Card.Title>Spotify Simple Clone</Card.Title>
+          <Card.Text>
+            Spotify Simple Clone é uma série de comandos e queries
+            que modela um banco de dados MySQL.
+          </Card.Text>
+          <div className="spotRepo">
+            <Card.Link
+              href="https://github.com/Tarcisio-Menezes/MySql-One-for-all"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Link para o repositório
+            </Card.Link>
+          </div>
+        </Card.Body>
+      </Card>
     </aside>
   );
 }
